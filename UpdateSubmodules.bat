5354bfbedd94e9cf6804753344decaa9f3af1a2d
a.fontverne <a.fontverne@student.isartdigital.com>
2025-10-03 19:02:09 +0200

    fix: remove submodules

--- UpdateSubmodules.bat
+++ UpdateSubmodules.bat
@@ -1,6 +1,23 @@
 @echo off
 setlocal enabledelayedexpansion
 
+for %%D in (
+    "IrisEngine/Externals/SPIRV-Reflect"
+    "IrisEngine/Externals/assimp"
+    "IrisEngine/Externals/stb"
+    "IrisEngine/Externals/glfw"
+    "IrisEngine/Externals/IrisEngine_Math"
+    "IrisEngine/Externals/json"
+    "IrisEngine/Externals/meta"
+) do (
+    if not exist %%D (
+        mkdir %%D
+        echo Created: %%D
+    ) else (
+        echo Already exists: %%D
+    )
+)
+
 git submodule init
 echo Submodule Init Done.
 
